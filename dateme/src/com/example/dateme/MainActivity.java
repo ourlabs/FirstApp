3addddcc7db4a6d98894b4753c3bb120729deb4e
RubbalSidhu <rubal90@gmail.com>
2012-10-19 00:38:41 +0530

    Integrate with FB

--- dateme/src/com/example/dateme/MainActivity.java
+++ dateme/src/com/example/dateme/MainActivity.java
@@ -1,20 +1,44 @@
 package com.example.dateme;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
 
 public class MainActivity extends Activity {
+
+    Facebook facebook = new Facebook("YOUR_APP_ID");
+    AsyncFacebookRunner mAsyncRunner = new AsyncFacebookRunner(facebook);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        facebook.authorize(this, new DialogListener() {
+            @Override
+            public void onComplete(Bundle values) {}
+
+            @Override
+            public void onFacebookError(FacebookError error) {}
+
+            @Override
+            public void onError(DialogError e) {}
+
+            @Override
+            public void onCancel() {}
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        facebook.authorizeCallback(requestCode, resultCode, data);
     }
 }
